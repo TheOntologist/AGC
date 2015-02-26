81bb522819e4e83797b6049d73b5de920e4c4104
niab <nikita.abramovs@gmail.com>
2015-02-26 22:39:57 +0000

    Added short keys for tabs resolves #37
    Alt+E - Events, Alt+C - Create Event, Alt+Q Quick Event, Alt+A Another
    Calendar

--- AGC.GUI/ViewModel/MainViewModel.cs
+++ AGC.GUI/ViewModel/MainViewModel.cs
@@ -16,6 +16,9 @@ namespace AGC.GUI.ViewModel
         #endregion
 
         public RelayCommand SelectEventsTabCommand { get; private set; }
+        public RelayCommand SelectCreateEventsTabCommand { get; private set; }
+        public RelayCommand SelectQuickEventsTabCommand { get; private set; }
+        public RelayCommand SelectAnotherCalendarTabCommand { get; private set; }
 
         #region Constructor
 
@@ -26,6 +29,9 @@ namespace AGC.GUI.ViewModel
                 log.Debug("Loading MainWindow view model...");
                 calendar = googleCalendar;
                 SelectEventsTabCommand = new RelayCommand(SelectEventsTab);
+                SelectCreateEventsTabCommand = new RelayCommand(SelectCreateEventsTab);
+                SelectQuickEventsTabCommand = new RelayCommand(SelectQuickEventsTab);
+                SelectAnotherCalendarTabCommand = new RelayCommand(SelectAnotherCalendarTab);
                 log.Debug("MainWindow view model was succssfully loaded");
             }
             catch(Exception ex)
@@ -58,9 +64,87 @@ namespace AGC.GUI.ViewModel
             }
         }
 
+        public const string CreateEventsTabSelectedPropertyName = "CreateEventsTabSelected";
+        private bool _createEventsTabSelected = false;
+        public bool CreateEventsTabSelected
+        {
+            get
+            {
+                return _createEventsTabSelected;
+            }
+
+            set
+            {
+                if (_createEventsTabSelected == value)
+                {
+                    return;
+                }
+
+                _createEventsTabSelected = value;
+                RaisePropertyChanged(CreateEventsTabSelectedPropertyName);
+            }
+        }
+
+        public const string QuickEventsTabSelectedPropertyName = "QuickEventsTabSelected";
+        private bool _quickEventsTabSelected = false;
+        public bool QuickEventsTabSelected
+        {
+            get
+            {
+                return _quickEventsTabSelected;
+            }
+
+            set
+            {
+                if (_quickEventsTabSelected == value)
+                {
+                    return;
+                }
+
+                _quickEventsTabSelected = value;
+                RaisePropertyChanged(QuickEventsTabSelectedPropertyName);
+            }
+        }
+
+        public const string AnotherCalendarTabSelectedPropertyName = "AnotherCalendarTabSelected";
+        private bool _anotherCalendarTabSelected = false;
+        public bool AnotherCalendarTabSelected
+        {
+            get
+            {
+                return _anotherCalendarTabSelected;
+            }
+
+            set
+            {
+                if (_anotherCalendarTabSelected == value)
+                {
+                    return;
+                }
+
+                _anotherCalendarTabSelected = value;
+                RaisePropertyChanged(AnotherCalendarTabSelectedPropertyName);
+            }
+        }
+        
         private void SelectEventsTab()
         {
             EventsTabSelected = true;
+        }
+
+        private void SelectCreateEventsTab()
+        {
+            CreateEventsTabSelected = true;
+        }
+
+        private void SelectQuickEventsTab()
+        {
+            QuickEventsTabSelected = true;
+        }
+
+        private void SelectAnotherCalendarTab()
+        {
+            AnotherCalendarTabSelected = true;
         }
     }
 }
